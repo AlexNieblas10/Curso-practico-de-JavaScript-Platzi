dae6def62b6f179b5b1c7a6e1556c7f482208808
Alex Nieblas <elalexnieblas@gmail.com>
2022-12-20 14:17:46 -0700

    Primera y segunda clase de manipulacion del DOM

---
+++ Manipulación del DOM/index.js
@@ -0,0 +1,15 @@
+const h1 = document.querySelector('p')
+const parrafo = document.querySelector('.parrafito')
+const pid = document.getElementById('pid')
+const input = document.querySelector('input')
+
+console.log(input.value);
+
+console.log({
+    h1,
+    parrafo,
+    pid,
+    input
+});
+
+
